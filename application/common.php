2db334e6a4d3532fac44ccbffddf6174878b2e73
JesBrian <2675862735@qq.com>
2017-09-14 21:28:12 +0800

    o2oeshop ver - 0.51
    添加了管理员主后台分类管理的各种功能

--- application/common.php
+++ application/common.php
@@ -10,3 +10,20 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+function status($status)
+{
+    if($status == 1)
+    {
+        $str = "<span class='label label-success radius'>正常</span>";
+    }
+    else if($status == 0)
+    {
+        $str = "<span class='label label-danger radius'>待审</span>";
+    }
+    else
+    {
+        $str = "<span class='label label-danger radius'>删除</span>";
+    }
+
+    return $str;
+}
---
+++ application/common/model/Category.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace app\common\model;
+
+use think\Model;
+
+class Category extends Model
+{
+    protected $autoWriteTimestamp = true;
+
+    /**
+     * 添加数据功能
+     */
+    public function add($data)
+    {
+        $data["status"] = 1;
+        return $this->save($data);
+    }
+
+    /**
+     * 获取正常一级分类的数据[不分页]，由于知识浅薄只能做到二级栏目
+     */
+    public function getNormalFirstCategory()
+    {
+        $data = [
+            "status"    =>  1,
+            "parent_id" =>  0,
+        ];
+        $order = [
+            "id"    =>  "desc",
+        ];
+        return $this->where($data)->order($order)->select();
+    }
+
+    /**
+     * 获取非删除分类的数据[分页]
+     */
+    public function getCategory($parentId)
+    {
+        $data = [
+            "status"    =>  ["NEQ", -1],
+            "parent_id" =>  $parentId,
+        ];
+        $order = [
+            "listorder"    =>  "desc",
+            "id"           =>  "asc"
+        ];
+        return $this->where($data)->order($order)->paginate();
+    }
+
+    /**
+     * 获取删除状态分类的数据[分页]
+     */
+    public function getDelCategory()
+    {
+        $data = [
+            "status"    =>  ["EQ", -1],
+        ];
+        $order = [
+            "listorder"    =>  "desc",
+            "id"           =>  "asc"
+        ];
+        return $this->where($data)->order($order)->paginate();
+    }
+}
